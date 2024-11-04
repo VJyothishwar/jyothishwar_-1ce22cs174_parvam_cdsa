c87064db9248e7821e2bf380219bb398c27b751a
VJyothishwar <jyothishwar08@gmail.com>
2024-11-04 11:44:17 +0530

    Add files via upload

---
+++ program-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    
+    int a=10;
+    printf("%d",a);
+
+     float b = 5.75;
+    double c = 19.99;
+
+    printf("The float value is: %f\n", b);
+    printf("The double value is: %lf\n", c);
+
+
+}
