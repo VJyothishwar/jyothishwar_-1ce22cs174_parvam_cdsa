c09b4eb009a3ec47160e6382e156e04514471431
VJyothishwar <jyothishwar08@gmail.com>
2024-11-08 19:16:41 +0530

    Add files via upload

--- max_arr.c
+++ max_arr.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 int main(){
-    int n,arr[5];
+    int n;
     printf("enter the number of elements");
     scanf("%d ",&n);
+    int arr[n];
+    int max=arr[0];
     printf("print  5 elements");
     for( int i=0; i<n; i++){
         scanf("%d ",&arr[i]);
     }
-        printf(" the array of elements arr...");//gjjhjuo909887786885
-
+       
         for (int i=0; i<=n; i++ ){
-            printf("%d ",arr[i]);
+            if (arr[i]>max){
+                max=arr[i];
+            }
         }
+
+            printf("the max element is %d ",max);
+        
 
     return 0;
 }
